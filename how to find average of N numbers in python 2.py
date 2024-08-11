e3df22f7bb57b1c32bb274e86f3cc8ecb635e95a
suresh1807 <sureshnalla175@gmail.com>
2024-08-11 22:47:39 +0530

    Add files via upload

---
+++ how to find average of N numbers in python 2.py
@@ -0,0 +1,27 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/sures/OneDrive/Documents/how to find average of N numbers in python.py
+how many numbers ? 3
+enter any numbers2
+enter any numbers3
+enter any numbers3
+average is  : 2.6666666666666665
+>>> 
+= RESTART: C:/Users/sures/OneDrive/Documents/how to find average of N numbers in python.py
+how many numbers ?5
+enter any numbers3
+enter any numbers4
+enter any numbers2
+enter any numbers4
+enter any numbers3
+average is  : 3.2
+>>> 
+= RESTART: C:/Users/sures/OneDrive/Documents/how to find average of N numbers in python.py
+how many numbers ?5
+enter any numbers4
+enter any numbers3
+enter any numbers2
+enter any numbers4
+enter any numbers6
+average is  : 3.8
